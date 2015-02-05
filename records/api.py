87be37eb4a467923e85c75099b15ebe400a4e5f8
Aleksey <veemer@yandex.ru>
2015-02-06 00:06:16 +0300

    create api for file uploads

--- records/api.py
+++ records/api.py
@@ -1,17 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from django.shortcuts import get_object_or_404
+
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from records.serializers import AttachmentSerializer
+from records.models import Record, Attachment
 
-class FileUploadView(APIView):
+
+class AttachmentsApiView(APIView):
+
     parser_classes = (FileUploadParser,)
 
-    def post(self, request, filename, format=None):
-        file_obj = request.data['file']
-        print request.data.keys()
-        # ...
-        # do some staff with uploaded file
-        # ...
-        return Response(status=204)
+    def post(self, request, **kwargs):
+
+        serializer = AttachmentSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            record_pk = kwargs.get('record_pk')
+            record = get_object_or_404(Record, pk=record_pk)
+            attachment = serializer.save(doctor=request.user, record=record)
+
+            data = {
+                'attachment': {
+                    'id': attachment.id,
+                    'url': attachment.attachment.url,
+                    'description': attachment.description
+                }
+            }
+            return Response(data, status=200)
+        
+        else:
+            data = {
+                'errors': serializer.errors
+            }
+
+            return Response(data, status=400)
