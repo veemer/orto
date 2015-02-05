902b0ff01043cee157fe53ec09a2c97c557a2bee
Aleksey <veemer@yandex.ru>
2015-02-06 00:05:38 +0300

    add Attachment to admin

--- records/admin.py
+++ records/admin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from records.models import Patient, Record
+from records.models import Patient, Record, Attachment
 
 admin.site.register(Patient)
 admin.site.register(Record)
+admin.site.register(Attachment)
