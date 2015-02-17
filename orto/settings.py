53dd0f4504cc4ad572ee875249a8a7a695aaf415
Aleksey <veemer@yandex.ru>
2015-02-17 23:17:43 +0300

    add request context processor

--- orto/settings.py
+++ orto/settings.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from django.conf import settings
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -89,3 +90,12 @@ LOGIN_REDIRECT_URL = '/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'records', 'media')
 MEDIA_URL = '/media/'
+
+TEMPLATE_CONTEXT_PROCESSORS = settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
+)
+
+try:
+    from production_settings import *
+except ImportError:
+    pass
