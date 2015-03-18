d9fafdb0180e0e89c1fd47c42fa2f8829c76f115
Aleksey <veemer@yandex.ru>
2015-03-18 20:45:17 +0300

    format serializers.py to pep8

--- records/serializers.py
+++ records/serializers.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import datetime
 from rest_framework import serializers
 from records.models import Attachment, Record
+
 
 class AttachmentSerializer(serializers.ModelSerializer):
 
