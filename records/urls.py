272ee1b816ebeec9e6eb45ec527529185b6e60c3
Aleksey <veemer@yandex.ru>
2015-03-18 20:40:38 +0300

    format records.urls to pep8

--- records/urls.py
+++ records/urls.py
@@ -1,61 +1,73 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
-from records.views import PatientList, CreatePatient, UpdatePatient, PatientBornTodayList, \
-                          PatientCsvList, PatientBornTodayCsvList, \
-                          RecordsList, CreateRecord, UpdateRecord, DetailRecord, CreateAttachments, \
-                          AgreementsList, AgreementDetail, AgreementCreate
+from records.views import PatientList, CreatePatient, UpdatePatient, PatientBornTodayList, PatientCsvList, \
+                          PatientBornTodayCsvList, RecordsList, CreateRecord, UpdateRecord, DetailRecord, \
+                          CreateAttachments, AgreementsList, AgreementDetail, AgreementCreate
 
 from records.api import AttachmentsApiView, RecordsApiView
 
 urlpatterns = patterns('',
 
-    # Patients views
+                       # Patients views
 
-    url(r'^$', login_required(PatientList.as_view()), name='patient_list'),
+                       url(r'^$', login_required(PatientList.as_view()), name='patient_list'),
 
-    url(r'^create_patient/$', CreatePatient.as_view(), name='create_patient'),
-    url(r'^update_patient/(?P<pk>\d+)/$', UpdatePatient.as_view(), name='update_patient'),
+                       url(r'^create_patient/$', login_required(CreatePatient.as_view()), name='create_patient'),
+                       url(r'^update_patient/(?P<pk>\d+)/$',
+                           login_required(UpdatePatient.as_view()), name='update_patient'),
 
-    url(r'^borntoday/$', PatientBornTodayList.as_view(), name='born_today'),
-    url(r'^patients/csv/$', PatientCsvList.as_view(), name='patient_list_csv'),
-    url(r'^borntoday/csv/$', PatientBornTodayCsvList.as_view(), name='born_today_csv'),
+                       url(r'^borntoday/$', login_required(PatientBornTodayList.as_view()), name='born_today'),
+                       url(r'^patients/csv/$', login_required(PatientCsvList.as_view()), name='patient_list_csv'),
+                       url(r'^borntoday/csv/$', login_required(PatientBornTodayCsvList.as_view()),
+                           name='born_today_csv'),
 
-    # Records views
+                       # Records views
 
-    url(r'^records/(?P<pk>\d+)/$', RecordsList.as_view(), name='records_list'),
-    url(r'^records/(?P<pk>\d+)/create/$', CreateRecord.as_view(), name='create_record'),
-    url(r'^records/(?P<pk>\d+)/update/$', UpdateRecord.as_view(), name='update_record'),
-    url(r'^records/(?P<pk>\d+)/detail/$', DetailRecord.as_view(), name='detail_record'),
-    url(r'^records/(?P<pk>\d+)/attach/$', CreateAttachments.as_view(), name='create_attachments'),
+                       url(r'^records/(?P<pk>\d+)/$', login_required(RecordsList.as_view()), name='records_list'),
+                       url(r'^records/(?P<pk>\d+)/create/$',
+                           login_required(CreateRecord.as_view()), name='create_record'),
+                       url(r'^records/(?P<pk>\d+)/update/$',
+                           login_required(UpdateRecord.as_view()), name='update_record'),
+                       url(r'^records/(?P<pk>\d+)/detail/$',
+                           login_required(DetailRecord.as_view()), name='detail_record'),
+                       url(r'^records/(?P<pk>\d+)/attach/$',
+                           login_required(CreateAttachments.as_view()), name='create_attachments'),
 
-    url(r'^print/$', TemplateView.as_view(template_name='records/print.html'), name='print'),
+                       url(r'^print/$', login_required(TemplateView.as_view(template_name='records/print.html')),
+                           name='print'),
 
-    # Agreements views
+                       # Agreements views
 
-    url(r'^agreements/(?P<pk>\d+)/$', AgreementsList.as_view(), name='agreements_list'),
-    url(r'^agreements/(?P<pk>\d+)/detail/$', AgreementDetail.as_view(), name='agreement_detail'),
-    url(r'^agreements/(?P<pk>\d+)/create/$', AgreementCreate.as_view(), name='agreement_create'),
+                       url(r'^agreements/(?P<pk>\d+)/$',
+                           login_required(AgreementsList.as_view()), name='agreements_list'),
+                       url(r'^agreements/(?P<pk>\d+)/detail/$',
+                           login_required(AgreementDetail.as_view()), name='agreement_detail'),
+                       url(r'^agreements/(?P<pk>\d+)/create/$',
+                           login_required(AgreementCreate.as_view()), name='agreement_create'),
 
-    # Accounts views
+                       # Accounts views
 
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'records/login.html'}),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+                       url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+                           {'template_name': 'records/login.html'}),
+                       url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+                           {'next_page': '/'}, name='logout'),
 
-    # api views
+                       # api views
 
-    url(r'^api/records/(?P<record_pk>\d+)/attachments$', AttachmentsApiView.as_view(), name='upload'),
-    url(r'^api/records/attachments$', AttachmentsApiView.as_view(), name='upload'),
+                       url(r'^api/records/(?P<record_pk>\d+)/attachments$',
+                           login_required(AttachmentsApiView.as_view()), name='upload'),
+                       url(r'^api/records/attachments$', login_required(AttachmentsApiView.as_view()), name='upload'),
 
-    url(r'^api/records/(?P<record_pk>\d+)$', RecordsApiView.as_view(), name='records'),
-    url(r'^api/records$', RecordsApiView.as_view(), name='records'),
+                       url(r'^api/records/(?P<record_pk>\d+)$',
+                           login_required(RecordsApiView.as_view()), name='records'),
+                       url(r'^api/records$', login_required(RecordsApiView.as_view()), name='records'),
 
-)
+                       )
 
-urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT}))
+urlpatterns += patterns('', (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                             {'document_root': settings.MEDIA_ROOT}))
