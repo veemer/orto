7495de5423a9380772131b0803f9a6bd5ef3401b
Aleksey <veemer@yandex.ru>
2015-03-18 23:19:24 +0300

    update migration

--- records/migrations/0007_auto_20150318_2119.py
+++ records/migrations/0007_auto_20150318_2119.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.hashers import make_password
 
 
 def create_groups(apps, schema_editor):
@@ -11,14 +12,17 @@ def create_groups(apps, schema_editor):
     technics_group = Group.objects.create(name='technics')
     managers_group = Group.objects.create(name='managers')
 
-    doctors = [User.objects.create(username='lukash', first_name=u'Лукаш Ю.В.', password='mb8zf'),
-               User.objects.create(username='volkova', first_name=u'Волкова Е.Н.', password='79cgt'),
-               User.objects.create(username='kislovskaya', first_name=u'Кисловская Е.Ю', password='zktj0'),
-               User.objects.create(username='ryabokonev', first_name=u'Рябоконев С.Г.', password='mgwpb'),
-               User.objects.create(username='vasilcova', first_name=u'Васильцова Д.В.', password='nxb1l')]
+    doctors = [User.objects.create(username='lukash', first_name=u'Лукаш Ю.В.', password=make_password('mb8zf')),
+               User.objects.create(username='volkova', first_name=u'Волкова Е.Н.', password=make_password('79cgt')),
+               User.objects.create(username='kislovskaya', first_name=u'Кисловская Е.Ю',
+                                   password=make_password('zktj0')),
+               User.objects.create(username='ryabokonev', first_name=u'Рябоконев С.Г.',
+                                   password=make_password('mgwpb')),
+               User.objects.create(username='vasilcova', first_name=u'Васильцова Д.В.',
+                                   password=make_password('nxb1l'))]
 
-    technic = User.objects.create(username='technic', password='gpzzk')
-    manager = User.objects.create(username='manager', password='1k2uw')
+    technic = User.objects.create(username='technic', password=make_password('gpzzk'))
+    manager = User.objects.create(username='manager', password=make_password('1k2uw'))
 
     doctors_group.user_set.add(*doctors)
     technics_group.user_set.add(technic)
