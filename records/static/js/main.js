5e73a700313444ba262de6d0622d67bf6e50d1b2
Aleksey <veemer@yandex.ru>
2015-03-21 02:41:38 +0300

    localization for bootstrap date picker

--- records/static/js/main.js
+++ records/static/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('#id_birth_day').datepicker({format: "dd.mm.yyyy", autoclose: true});
-    $('#id_next_visit_date').datepicker({format: "dd.mm.yyyy", autoclose: true});
+    $('#id_birth_day').datepicker({format: "dd.mm.yyyy", autoclose: true, language: "ru"});
+    $('#id_next_visit_date').datepicker({format: "dd.mm.yyyy", autoclose: true, language: "ru"});
 
 });
