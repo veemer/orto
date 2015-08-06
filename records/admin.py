1c71269637547bef82c741ef8266f76d8ec5e1d7
Aleksey <veemer@yandex.ru>
2015-08-07 00:36:12 +0300

    register agreements models in admin

--- records/admin.py
+++ records/admin.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from records.models import Patient, Record, Attachment
+from records.models import Patient, Record, Attachment, Agreement, PhysioAgreement
 
 admin.site.register(Patient)
 admin.site.register(Record)
 admin.site.register(Attachment)
+admin.site.register(Agreement)
+admin.site.register(PhysioAgreement)
